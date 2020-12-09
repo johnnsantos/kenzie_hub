b715ee468f7babac0825b6d6cc2e9586616984bb
christian sequeira <csequeira153@gmail.com>
2020-12-08 21:28:14 -0300

    feat: header while logged out
    Header showed in while user is logged out

--- src/Routers/index.js
+++ src/Routers/index.js
@@ -1,11 +1,13 @@
 import { Route, Switch, useHistory } from "react-router-dom";
+import Header from "../components/header/index.jsx";
 
 const Routers = () => {
   return (
     <>
       <Switch>
         <Route exact path="/">
-          <h1>Bora Codar</h1>
+          {/* <h1>Bora Codar</h1> */}
+          <Header />
         </Route>
       </Switch>
     </>
