25d32a32c83a37cc2b138b04652c43e5390f962a
Yuri Baldini <yuriabaldini@gmail.com>
2020-12-09 16:54:04 -0300

    feature: sendForm now linked with dispatch

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,8 +1,9 @@
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-//import { useDispatch } from "react-redux";
-import "./style.css";
+import { useDispatch } from "react-redux";
+import "./register.css";
+import { signUpUser } from "requests";
 
 const Register = () => {
   //const dispatch = useDispatch();
@@ -46,7 +47,10 @@ const Register = () => {
     resolver: yupResolver(schema),
   });
 
-  const sendForm = () => {};
+  const sendForm = (data) => {
+    delete data.password_confirmation;
+    dispatch(signUpUser(data));
+  };
 
   return (
     <div className="render">
