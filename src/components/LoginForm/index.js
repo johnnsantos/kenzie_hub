e840b3da77035817644a6c99871c0c6af2396d21
Yuri Baldini <yuriabaldini@gmail.com>
2020-12-10 17:31:32 -0300

    fix: fix on login

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,43 +1,80 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import LoginForm from "../../components/LoginForm";
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import backgroundLogin from "../../img/login.svg";
-import { StyledDiv, StyledImg } from "./styles";
+import { useForm } from "react-hook-form";
+import { login } from "../../requests";
 
+import { NewTypography, NewTextField, OuterDiv, StyledButton } from "./styles";
+import axios from "axios";
 const baseURL = "https://kenziehub.me";
-const Login = () => {
-  const [isAuthenticated, setAuthentication] = useState(undefined);
+
+const LoginForm = ({ setAuthentication }) => {
   const history = useHistory();
+  const [message, setMessage] = useState("");
+
+  const {
+    register,
+    unregister,
+    handleSubmit,
+    setValue,
+    errors,
+    setError,
+  } = useForm();
+
   useEffect(() => {
-    const authToken = window.localStorage.getItem("authToken");
-    if (!authToken) {
-      setAuthentication(false);
-    }
-    axios
-      .get(`${baseURL}/sessions`, {
-        headers: { Authorization: `Bearer ${authToken}` },
-      })
-      .then(() => {
-        setAuthentication(true);
-        //mandar para a pagina de usuários
-      })
-      .catch(() => {
-        setAuthentication(false);
-      });
-  }, [history, setAuthentication]); //ver oque preciso passar como array de dependencia
-  if (isAuthenticated === undefined) {
-    return <div> Loading ...</div>;
-  }
-  if (isAuthenticated === false) {
-    return (
-      <StyledDiv>
-        <StyledImg src={backgroundLogin} />
-        <LoginForm setAuthentication={setAuthentication} />
-      </StyledDiv>
-    );
-  }
-  //rotas autenticadas
+    register("email", { required: "O email está em branco" });
+    register("password", { required: "A senha está em branco" });
+    return () => {
+      unregister("email");
+      unregister("passoword");
+    };
+  }, [register, unregister]);
+
+  const handleLogin = async (data) => {
+    const resLogin = await login(data);
+    setMessage(resLogin);
+    history.push("/devs");
+  };
+
+  return (
+    <OuterDiv>
+      <NewTypography variant="h3">Login</NewTypography>
+      {message}
+      <form onSubmit={handleSubmit(handleLogin)}>
+        <div>
+          <NewTextField
+            variant="outlined"
+            margin="normal"
+            size="medium"
+            label="Usuário"
+            name="email"
+            inputRef={register}
+            onChange={(e) => setValue("email", e.target.value)}
+          />
+          {errors.user && (
+            <p style={{ color: "red" }}> {errors.user.message} </p>
+          )}
+        </div>
+        <div>
+          <NewTextField
+            margin="normal"
+            variant="outlined"
+            size="medium"
+            label="Senha"
+            name="password"
+            inputRef={register}
+            onChange={(e) => setValue("password", e.target.value)}
+          />
+          {errors.password && (
+            <p style={{ color: "red" }}>{errors.password.message}</p>
+          )}
+        </div>
+        <StyledButton color="primary" variant="contained" type="submit">
+          {" "}
+          Entrar
+        </StyledButton>
+      </form>
+    </OuterDiv>
+  );
 };
 
-export default Login;
+export default LoginForm;
