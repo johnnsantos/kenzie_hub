e7af11acb7dd094207f5b14c9f061728043e6137
Johnnatan <contatojohnnatan@gmail.com>
2020-12-11 15:15:33 -0300

    feat(profilepage): created page and responsive works

---
+++ src/pages/profilePage/index.jsx
@@ -0,0 +1,52 @@
+import { Typography } from "@material-ui/core";
+import { StyledAvatar } from "./styles";
+import "./styles.css";
+
+const ProfilePage = () => {
+  return (
+    <div className="root">
+      <div className="profile">
+        <StyledAvatar />
+        <Typography variant="h5">Johnnatan Santos</Typography>
+        <div className="profile-data">
+          <Typography variant="h6">Email: johndoe@email.com</Typography>
+          <Typography variant="h6">
+            Módulo: Segundo Módulo (Frontend avançado)
+          </Typography>
+        </div>
+        <div className="profile-about">
+          <Typography variant="body1">
+            Sobre: Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Aliquid dolorem officia, a non placeat aperiam est ducimus
+            laudantium nemo fugiat quo quis voluptates corrupti sunt natus
+            perspiciatis voluptatum vero adipisci?
+          </Typography>
+        </div>
+      </div>
+      <div className="section">
+        <div className="technologies-title">
+          <Typography variant="h6">Tecnologias</Typography>
+        </div>
+        <ul>
+          <li>Test</li>
+          <li>test</li>
+          <li>test</li>
+          <li>test</li>
+          <li>test</li>
+        </ul>
+        <div className="technologies-title">
+          <Typography variant="h6">Trabalhos</Typography>
+        </div>
+        <ul>
+          <li>Test</li>
+          <li>test</li>
+          <li>test</li>
+          <li>test</li>
+          <li>test</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
---
+++ src/pages/profilePage/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { Avatar } from "@material-ui/core";
+
+export const StyledAvatar = styled(Avatar)`
+  width: 120px;
+  height: 120px;
+  background-color: #5e7fbe;
+`;
