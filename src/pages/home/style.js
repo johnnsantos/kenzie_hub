e6c1ced11a542875fafb0d966433784ac67a61a5
Christian Sequeira <csequeira153@gmail.com>
2020-12-15 15:43:32 -0300

    style(home): changed banner style

--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -8,13 +8,15 @@ export const Container = styled.div`
   flex-wrap: wrap;
   justify-content: space-around;
   align-items: center;
-
-  background-color: rgba(53, 74, 95, 0.2);
-  border-radius: 3px;
-
+  /* background-color: rgba(53, 74, 95, 0.2); */
+  /* border-radius: 3px; */
   padding: 2rem 1rem;
-
   margin: 2rem 0;
+
+  background-color: rgba(229, 231, 247, 0.3);
+  border-radius: 90px 90px 20px 20px;
+  box-shadow: 0px 15px 18px -9px rgba(0, 0, 0, 0.75);
+
   img {
     width: 350px;
     margin: 1rem;
@@ -25,6 +27,11 @@ export const Container = styled.div`
     flex-direction: column;
     align-items: center;
     text-align: left;
+
+    background-color: rgba(229, 231, 247, 0.8);
+    border-radius: 29% 71% 34% 66% / 40% 66% 34% 60%;
+    border: 0px solid #000000;
+    padding: 1rem;
 
     span {
       color: rgba(6, 147, 11, 0.9);
@@ -49,19 +56,15 @@ export const Container = styled.div`
     button {
       text-align: center;
       font-size: 21px;
-
-      width: 180px;
-      height: 40px;
+      width: 200px;
+      height: 60px;
       margin-top: 1rem;
-
       color: white;
-
       border: none;
-
       background-color: #2a3b6f;
-      border-radius: 2px;
-
+      border-radius: 5px;
       cursor: pointer;
+      font-weight: bold;
 
       :hover {
         background-color: #425dae;
@@ -70,11 +73,20 @@ export const Container = styled.div`
   }
 
   @media (min-width: 768px) {
-    width: 80%;
+    width: 95%;
     margin: 2rem auto;
 
+    .bannerText > .h1Logged {
+      font-size: 50px;
+    }
+
+    .bannerText > .h2Logged {
+      font-size: 40px;
+      color: #3f3d56;
+    }
+
     img {
-      width: 450px;
+      width: 650px;
       margin: 1rem;
     }
   }
